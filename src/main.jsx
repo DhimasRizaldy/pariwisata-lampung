301adc918cf6dead5cdd4e0e5c705a734b470c94
DhimasRizaldy <dtekchannel02@gmail.com>
2023-12-27 13:23:36 +0700

    main:updatedeploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "../src/assets/dist/css/app.css";
 import HomeUser from "./pages/Home/HomeUser.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <HomeUser />
+    <HomeUser />
   </React.StrictMode>
 );
