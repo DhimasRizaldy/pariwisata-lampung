c99f23763067d6dc867a41cc8ec514abae55a0a7
DhimasRizaldy <dtekchannel02@gmail.com>
2023-12-30 23:28:05 +0700

    main:update

--- src/pages/Users/DashboardUser/ContentUser.jsx
+++ src/pages/Users/DashboardUser/ContentUser.jsx
@@ -14,8 +14,85 @@ class ContentUser extends Component {
           <div className="intro-y flex items-center justify-center mt-10 mb-10">
             <h2 className="text-lg font-bold">Wisata Alam Lampung</h2>
           </div>
+          <div class="flex items-center justify-between bg-gray-100 p-4">
+            <div class="relative">
+              <input
+                type="text"
+                class="border rounded-md py-2 px-4 w-64 focus:outline-none focus:border-blue-500"
+                placeholder="Cari Wisata Alam ..."
+              />
+              <button class="absolute right-0 top-0 mt-3 mr-4">
+                <i data-feather="search" class="text-gray-500"></i>
+              </button>
+            </div>
+          </div>
           <div className="intro-y grid grid-cols-12 gap-6 mt-5">
             <Dashboard />
+            {/* pagination */}
+            <div class="intro-y col-span-12 flex flex-wrap sm:flex-row sm:flex-no-wrap items-center">
+              <ul class="pagination">
+                <li>
+                  <a class="pagination__link" href="">
+                    {" "}
+                    <i class="w-4 h-4" data-feather="chevrons-left"></i>{" "}
+                  </a>
+                </li>
+                <li>
+                  <a class="pagination__link" href="">
+                    {" "}
+                    <i class="w-4 h-4" data-feather="chevron-left"></i>{" "}
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a class="pagination__link" href="">
+                    ...
+                  </a>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <a class="pagination__link" href="">
+                    1
+                  </a>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <a class="pagination__link pagination__link--active" href="">
+                    2
+                  </a>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <a class="pagination__link" href="">
+                    3
+                  </a>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <a class="pagination__link" href="">
+                    ...
+                  </a>{" "}
+                </li>
+                <li>
+                  <a class="pagination__link" href="">
+                    {" "}
+                    <i class="w-4 h-4" data-feather="chevron-right"></i>{" "}
+                  </a>
+                </li>
+                <li>
+                  <a class="pagination__link" href="">
+                    {" "}
+                    <i class="w-4 h-4" data-feather="chevrons-right"></i>{" "}
+                  </a>
+                </li>
+              </ul>
+              <select class="w-20 input box mt-3 sm:mt-0">
+                <option>10</option>
+                <option>25</option>
+                <option>35</option>
+                <option>50</option>
+              </select>
+            </div>
           </div>
         </div>
       </Fragment>
